6c629f021f1ce4c498c5bd4ffbcc2d5fc57788bc
Mahdi Rahmani <mahdirahmani.main@gmail.com>
2024-08-18 19:07:42 +0330

    delete bookmark

--- src/components/context/bookmarkListContext.jsx
+++ src/components/context/bookmarkListContext.jsx
@@ -51,6 +51,18 @@ function BookmarkListProvider({ children }) {
     }
   }
 
+  async function deleteBookmark(id) {
+    setIsLoading(true);
+    try {
+      await axios.delete(`${BASE_URL}/bookmarks/${id}`);
+      setBookmarks((prev) => prev.filter((item) => item.id !== id));
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
     <BookmarkContext.Provider
       value={{
@@ -58,8 +70,8 @@ function BookmarkListProvider({ children }) {
         bookmarks,
         currentBookmark,
         getBookmark,
-        isLoading,
         createBookmark,
+        deleteBookmark,
       }}
     >
       {children}
